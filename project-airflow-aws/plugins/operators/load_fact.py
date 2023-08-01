b23b8dd595e52bb96b301e8779499ba260e5db91
Samuel Martins <sbm.martins@gmail.com>
2023-08-01 14:35:42 -0300

    feat: adding an option to truncate the table

--- project-airflow-aws/plugins/operators/load_fact.py
+++ project-airflow-aws/plugins/operators/load_fact.py
@@ -10,6 +10,7 @@ class LoadFactOperator(BaseOperator):
                  redshift_conn_id='',
                  table='',
                  sql_query='',
+                 truncate_table=False,
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
@@ -17,10 +18,15 @@ class LoadFactOperator(BaseOperator):
         self.redshift_conn_id = redshift_conn_id
         self.table = table
         self.sql_query = sql_query
+        self.truncate_table = truncate_table
 
 
     def execute(self, context):
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        if self.truncate_table:
+            self.log.info("\n\n### Deleting data from Fact Table")
+            redshift.run(f"TRUNCATE {self.table}")
 
         self.log.info(f'\n\n### Loading Fact Table')
         redshift.run(f"INSERT INTO {self.table} {self.sql_query}")
